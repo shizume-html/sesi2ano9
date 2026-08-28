9f8fbe697a828cfef9121f5ba522ad57ebd535f6
shizume-html <richard.f.dias@aluno.senai.br>
2026-08-28 16:29:37 -0300

    s
    s

---
+++ aula 2- C++/Untitled5.cpp
@@ -0,0 +1,17 @@
+#include <studio.h>
+
+int main(){
+	
+	float peso, altura, imc;
+	
+	printf("digite seu peso em kg:");
+	scanf"%f"", &peso);
+	
+	printf("Digite sua altura em mentros:")
+	scanf("%f", &altura);
+	
+	imc= peso/ (altura * altura);
+	
+	printf("\Seu IMC e: %.2f\n", imc);
+	return 0;
+}
---
+++ aula 2- C++/projeto.cpp
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+int main(){
+	int x; 
+	
+	print("digite um numero");
+	scanf("% d , &x");
+	printf("Valor digitado: % d ,x);")
+	prinf("valor digitado:%
+	
+}
